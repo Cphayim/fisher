b5b1f80efa7448abba3df6afc2374021491665fa
Cphayim <admin@cphayim.me>
2018-06-28 20:21:05 +0800

    feat(spider): YuShuBook 添加 first 属性

--- app/spider/yushu_book.py
+++ app/spider/yushu_book.py
@@ -69,3 +69,7 @@ class YuShuBook:
         :return:
         """
         return (page - 1) * current_app.config['PER_PAGE']
+
+    @property
+    def first(self):
+        return self.books[0] if self.total >= 1 else None
