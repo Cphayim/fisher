33ec8f20144971aa94701b01e46c28031a1d17e4
Cphayim <admin@cphayim.me>
2018-06-29 13:41:15 +0800

    perf(form): 优化代码结构

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -10,15 +10,15 @@ from models.user import User
 __author__ = 'Cphayim'
 
 
-class RegisterForm(Form):
+class LoginForm(Form):
     """
-    注册表单验证
+    登录表单验证器
     """
     # 电子邮箱
     email = StringField(
         validators=[
-            DataRequired(),
-            Length(8, 64),
+            DataRequired(message='电子邮箱不可以为空'),
+            Length(8, 64, message='电子邮箱不符合规范'),
             Email(message='电子邮箱不符合规范')
         ]
     )
@@ -26,16 +26,8 @@ class RegisterForm(Form):
     # 密码
     password = PasswordField(
         validators=[
-            DataRequired(message='密码不可以为空，请输入你的密码'),
-            Length(6, 32)
-        ]
-    )
-
-    # 昵称
-    nickname = StringField(
-        validators=[
-            DataRequired(),
-            Length(2, 10, message='昵称至少需要两个字符，最多10个字符')
+            DataRequired(message='密码不可以为空'),
+            Length(6, 32, message='密码长度应该在6~32位')
         ]
     )
 
@@ -44,23 +36,20 @@ class RegisterForm(Form):
         if User.query.filter_by(email=field.data).first():
             raise ValidationError('电子邮箱已被注册')
 
+
+class RegisterForm(LoginForm):
+    """
+    注册表单验证器
+    """
+
+    # 昵称
+    nickname = StringField(
+        validators=[
+            DataRequired('昵称不可以为空'),
+            Length(2, 10, message='昵称长度应该在2~10个字符')
+        ]
+    )
+
     def validate_nickname(self, field):
         if User.query.filter_by(nickname=field.data).first():
             raise ValidationError('该昵称已被使用')
-
-
-class LoginForm(Form):
-    email = StringField(
-        validators=[
-            DataRequired(),
-            Length(8, 64),
-            Email()
-        ]
-    )
-
-    password = PasswordField(
-        validators=[
-            DataRequired(),
-            Length(6, 32)
-        ]
-    )
