9111207e47a6572ea83d29cc0d010c25dd52ac14
Cphayim <admin@cphayim.me>
2018-06-30 21:37:06 +0800

    feat(model): 自定义 SQLAlchemy 子类，实现自动 commit
    * models/base.py -> SQLAlchemy
      * 继承原 SQLAlchemy 类，实现一个返回自动 commit 的上下文管理器的方法
    * models/base.py -> Base
      * 在构造函数中为 create_time 赋值

--- app/models/base.py
+++ app/models/base.py
@@ -2,10 +2,29 @@
 """
   Created by Cphayim at 2018/6/28 20:23
 """
-from flask_sqlalchemy import SQLAlchemy
+from contextlib import contextmanager
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy as _SQLAlchemy
 from sqlalchemy import Column, Integer, SmallInteger
 
 __author__ = 'Cphayim'
+
+
+# SQLAlchemy 的子类，实现一个自动 commit 的上下文管理器
+class SQLAlchemy(_SQLAlchemy):
+    @contextmanager
+    def auto_commit(self):
+        try:
+            yield
+            # 事务
+            # with 语句块内代码执行完毕后自动 commit
+            self.session.commit()
+        except Exception as e:
+            # 若插入出现异常，执行回滚
+            self.session.rollback()
+            raise e
+
 
 # 初始化 SQLAlchemy
 db = SQLAlchemy()
@@ -20,6 +39,9 @@ class Base(db.Model):
     create_time = Column('create_time', Integer)
     # 当前状态 1: 存在
     status = Column(SmallInteger, default=1)
+
+    def __init__(self):
+        self.create_time = int(datetime.now().timestamp())
 
     def set_attrs(self, attrs_dict):
         """
