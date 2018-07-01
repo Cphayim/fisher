d592714a5b52ccbcf93747791ee3dd0399427ef4
Cphayim <admin@cphayim.me>
2018-07-01 23:47:08 +0800

    feat(model): 心愿清单数据模型
    * models/wish.py
      * 心愿清单数据模型
    * models/gift.py
      * 注释修改

--- app/models/wish.py
+++ app/models/wish.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-  Created by Cphayim at 2018/6/28 20:22
+  Created by Cphayim at 2018/6/30 18:11
 """
-from sqlalchemy import Column, Integer, Boolean, ForeignKey, String, SmallInteger
+from sqlalchemy import Column, Integer, ForeignKey, String, Boolean
 from sqlalchemy.orm import relationship
 
 from models.base import Base
@@ -10,9 +10,9 @@ from models.base import Base
 __author__ = 'Cphayim'
 
 
-class Gift(Base):
+class Wish(Base):
     """
-    赠送清单（赠书）模型
+    心愿清单（索书）模型
     """
     # id
     id = Column(Integer, primary_key=True)
@@ -20,10 +20,7 @@ class Gift(Base):
     user = relationship('User')
     # 用户 id（外键）
     uid = Column(Integer, ForeignKey('user.id'))
-    # 数据库中没有存 book 的数据（数据来自鱼书API）
-    # book = relationship(Book)
-    # bid = Column(Integer, ForeignKey('book.id'))
     # isbn 编号
     isbn = Column(String(15), nullable=False)
-    # 是否已送出
+    # 是否已收到
     launched = Column(Boolean, default=False)
