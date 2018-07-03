1bf8defd513900d9be7e3684e04bc8ccf8c416a7
Cphayim <admin@cphayim.me>
2018-07-03 09:49:36 +0800

    feat(web): 统一404处理
    - web/__init__.py
      - app_errorhandler 404

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -2,11 +2,18 @@
 """
   Created by Cphayim at 2018/6/24 18:53
 """
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 __author__ = 'Cphayim'
 
 # 创建蓝图实例
 web = Blueprint('web', __name__)
+
+
+@web.app_errorhandler(404)
+def not_found(e):
+    # AOP
+    return render_template('404.html'), 404
+
 
 from web import book, auth, drift, gift, main, wish, test
